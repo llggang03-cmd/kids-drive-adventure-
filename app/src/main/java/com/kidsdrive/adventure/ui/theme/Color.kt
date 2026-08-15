2ca32c1aa29fd597167cf7dc2b2b60cfe1df4a93
llggang03-cmd <llggang03@gmail.com>
2026-08-15 10:58:55 +0530

    Create Color.kt

---
+++ app/src/main/java/com/kidsdrive/adventure/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.kidsdrive.adventure.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SkyDayTop = Color(0xFF7EC8F2)
+val SkyDayBottom = Color(0xFFCDEFFF)
+val SkyNightTop = Color(0xFF0D1B3E)
+val SkyNightBottom = Color(0xFF2C3E6B)
+
+val RoadGray = Color(0xFF4A4A55)
+val RoadLine = Color(0xFFFFF7C2)
+
+val GrassGreenDay = Color(0xFF7ED957)
+val GrassGreenNight = Color(0xFF2E4A2E)
+
+val PrimaryOrange = Color(0xFFFF7A33)
+val PrimaryOrangeDark = Color(0xFFE85D1F)
+val AccentYellow = Color(0xFFFFC93C)
+val CoinGold = Color(0xFFFFD54F)
+
+val SoftWhite = Color(0xFFFFFDF7)
+val TextDark = Color(0xFF2B2B36)
+val CardGray = Color(0xFFF3F3F7)
